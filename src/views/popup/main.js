dbbabe9032cb2a75507ae223234f5e3aa6f87f26
8788 <2802513+8788@users.noreply.github.com>
2019-01-31 13:20:23 +0800

    remove unused modules

--- src/views/popup/main.js
+++ src/views/popup/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './index.vue'
 import '../../assets/128.png'
-import VCharts from 'v-charts'
 import ViserVue from 'viser-vue'
 
 Vue.config.productionTip = false
 
-Vue.use(VCharts)
 Vue.use(ViserVue)
 
 new Vue({
